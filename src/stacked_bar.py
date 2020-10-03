d5501398a1a650281e3b93c00569a8331cf38a58
Felix Brilej <felix@globaldatanet.com>
2020-10-03 18:27:02 +0200

    make the stacked bar dynamic

--- src/stacked_bar.py
+++ src/stacked_bar.py
@@ -4,44 +4,54 @@ import matplotlib.pyplot as plt
 from matplotlib import rc
 import pandas as pd
 
+# Thanks to Olivier Gaudard for this example! https://python-graph-gallery.com/12-stacked-barplot-with-matplotlib/
+
 
 # nice colors
 yellow = "#fed919"
 cyan = "#2db2a1"
 pink = "#ff6f61"
-purple = "#a41d53"
+magenta = "#a41d53"
+purple = "#6a45ce"
 green = "#3ecc54"
-colors = [purple, green, pink, yellow, cyan]
+colors = [purple, green, pink, yellow, magenta, cyan]
 
-# y-axis in bold
-rc('font', weight='bold')
- 
+
+def draw_bars(all_bars):
+    # The position of the bars on the x-axis
+    r = range(len(all_bars[0]))
+    # Names of group and bar width (assuming the data starts at the first of the month)
+    names = [str(x + 1) for x in r]
+    barWidth = 0.5
+
+    # Draw bars
+    bottom = [0 for x in all_bars[0]]
+    edgecolor = "white"
+    for x in range(len(all_bars)):
+        # There is nothing below the first bar, skip it for the first iteration
+        if x > 0:
+            bottom = np.add(bottom, all_bars[x - 1]).tolist()
+
+        plt.bar(r, all_bars[x], bottom=bottom, color=colors[x], edgecolor=edgecolor, width=barWidth)
+
+    # Custom X axis
+    plt.xticks(r, names, fontweight='bold')
+    plt.xlabel("Days")
+    plt.ylabel("Cost")
+
+    # y-axis in bold
+    rc('font', weight='bold')
+
+    # Show graphic
+    plt.show()
+
+
 # Values of each group
-bars1 = [12, 28, 1, 8, 22]
-bars2 = [28, 7, 16, 4, 10]
-bars3 = [25, 3, 23, 25, 17]
- 
-# Heights of bars1 + bars2
-bars = np.add(bars1, bars2).tolist()
- 
-# The position of the bars on the x-axis
-r = [0,1,2,3,4]
- 
-# Names of group and bar width
-names = ['1','2','3','4','5']
-barWidth = 0.5
- 
-# Create brown bars
-plt.bar(r, bars1, color=colors[0], edgecolor='white', width=barWidth)
-# Create green bars (middle), on top of the firs ones
-plt.bar(r, bars2, bottom=bars1, color=colors[1], edgecolor='white', width=barWidth)
-# Create green bars (top)
-plt.bar(r, bars3, bottom=bars, color=colors[2], edgecolor='white', width=barWidth)
- 
-# Custom X axis
-plt.xticks(r, names, fontweight='bold')
-plt.xlabel("Days")
-plt.ylabel("Cost")
- 
-# Show graphic
-plt.show()
+all_bars = [
+    [12, 28, 1, 8, 22, 21, 12],
+    [28, 7, 16, 4, 10, 11, 14],
+    [25, 3, 23, 25, 17, 2, 2],
+    [25, 3, 23, 25, 17, 2, 2]
+]
+
+draw_bars(all_bars)
