50b2e06b14383da25f529c563d08e13e4231b5c6
Felix Brilej <felix@globaldatanet.com>
2020-10-03 19:34:55 +0200

    fix data format

--- src/lambda.py
+++ src/lambda.py
@@ -1,15 +1,16 @@
 import boto3
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
-
+import stacked_bar
 
 def lambda_handler(event, context):
     client = boto3.client('ce')
     today = datetime.today().strftime('%Y-%m-%d')
     first_day_of_month = datetime.today().replace(day=1).strftime('%Y-%m-%d')
+    start_date = (datetime.now() - timedelta(days=10)).strftime('%Y-%m-%d')
     results = client.get_cost_and_usage(
         TimePeriod={
-            'Start': first_day_of_month,
+            'Start': start_date, #first_day_of_month,
             'End': today
         },
         Granularity='DAILY',
@@ -29,17 +30,22 @@ def lambda_handler(event, context):
     # Prep the data for the graphing
     # Get the list of services
     #services = [x["Keys"][0] for x in results[0]["Groups"]]
-    services = ['AWS Transfer Family', 'AWS Lambda', 'Amazon Relational Database Service', 'Tax', 'Amazon DynamoDB']
+    services = ['AWS Transfer Family', 'AWS Lambda', 'Amazon Relational Database Service', 'Amazon DynamoDB']
     graph_data = {service: [] for service in services}
     for service in services:
         for day in results:
+            found_service = False
             for group in day["Groups"]:
                 if service in group["Keys"]:
-                    graph_data[service].append(group["Metrics"]["AmortizedCost"]["Amount"])
+                    graph_data[service].append(float(group["Metrics"]["AmortizedCost"]["Amount"]))
+                    found_service = True
                     break
+            # Fill up a 0 if service doesnt have cost for that day
+            if not found_service:
+                graph_data[service].append(0)
 
-    print(json.dumps(graph_data, indent=4))
-
+    #print(json.dumps(graph_data, indent=4))
+    stacked_bar.draw_bars(graph_data)
     #print(json.dumps(results, indent=4))
 
 
--- src/stacked_bar.py
+++ src/stacked_bar.py
@@ -2,7 +2,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rc
-import pandas as pd
 
 # Thanks to Olivier Gaudard for this example! https://python-graph-gallery.com/12-stacked-barplot-with-matplotlib/
 
@@ -14,30 +13,38 @@ pink = "#ff6f61"
 magenta = "#a41d53"
 purple = "#6a45ce"
 green = "#3ecc54"
-colors = [purple, green, pink, yellow, magenta, cyan]
+COLORS = [purple, green, pink, yellow, magenta, cyan]
 
 
 def draw_bars(all_bars):
+    first_key = list(all_bars.keys())[0]
+    days = len(all_bars[first_key])
     # The position of the bars on the x-axis
-    r = range(len(all_bars[0]))
+    r = range(days)
     # Names of group and bar width (assuming the data starts at the first of the month)
     names = [str(x + 1) for x in r]
     barWidth = 0.5
 
     # Draw bars
-    bottom = [0 for x in all_bars[0]]
+    bottom = [0 for x in r]
     edgecolor = "white"
-    for x in range(len(all_bars)):
+    x = 0
+    for label in all_bars.keys():
         # There is nothing below the first bar, skip it for the first iteration
         if x > 0:
-            bottom = np.add(bottom, all_bars[x - 1]).tolist()
+            print(previous_bar)
+            bottom = np.add(bottom, previous_bar).tolist()
 
-        plt.bar(r, all_bars[x], bottom=bottom, color=colors[x], edgecolor=edgecolor, width=barWidth)
+        plt.bar(r, all_bars[label], bottom=bottom, color=COLORS[x], edgecolor=edgecolor, width=barWidth, label=label)
+        x += 1
+        previous_bar = all_bars[label]
 
     # Custom X axis
     plt.xticks(r, names, fontweight='bold')
     plt.xlabel("Days")
     plt.ylabel("Cost")
+
+    plt.legend()
 
     # y-axis in bold
     rc('font', weight='bold')
@@ -46,12 +53,13 @@ def draw_bars(all_bars):
     plt.show()
 
 
-# Values of each group
-all_bars = [
-    [12, 28, 1, 8, 22, 21, 12],
-    [28, 7, 16, 4, 10, 11, 14],
-    [25, 3, 23, 25, 17, 2, 2],
-    [25, 3, 23, 25, 17, 2, 2]
-]
+if __name__ == "__main__":
+    # Values of each group
+    all_bars = {
+        "Lambda": [12, 28, 1, 8, 22, 21, 12],
+        "EC2": [28, 7, 16, 4, 10, 11, 14],
+        "Salt": [25, 3, 23, 25, 17, 2, 2],
+        "Pepper": [25, 3, 23, 25, 17, 2, 2]
+    }
 
-draw_bars(all_bars)
+    draw_bars(all_bars)
