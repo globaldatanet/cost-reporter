264899f706597ea1de02261a90d13ef24be78b28
Felix Brilej <felix@globaldatanet.com>
2020-10-03 18:52:16 +0200

    add preparation of data for plotting

--- src/lambda.py
+++ src/lambda.py
@@ -2,6 +2,7 @@ import boto3
 from datetime import datetime
 import json
 
+
 def lambda_handler(event, context):
     client = boto3.client('ce')
     today = datetime.today().strftime('%Y-%m-%d')
@@ -11,7 +12,7 @@ def lambda_handler(event, context):
             'Start': first_day_of_month,
             'End': today
         },
-        Granularity='MONTHLY',
+        Granularity='DAILY',
         Metrics=[
             'AmortizedCost',
         ],
@@ -22,6 +23,24 @@ def lambda_handler(event, context):
             }
         ]
     )["ResultsByTime"]
-    print(json.dumps(results, indent=4))
+
+    # TODO find the biggest spenders so far. for this, issue a query and sort by MONTHLY
+
+    # Prep the data for the graphing
+    # Get the list of services
+    #services = [x["Keys"][0] for x in results[0]["Groups"]]
+    services = ['AWS Transfer Family', 'AWS Lambda', 'Amazon Relational Database Service', 'Tax', 'Amazon DynamoDB']
+    graph_data = {service: [] for service in services}
+    for service in services:
+        for day in results:
+            for group in day["Groups"]:
+                if service in group["Keys"]:
+                    graph_data[service].append(group["Metrics"]["AmortizedCost"]["Amount"])
+                    break
+
+    print(json.dumps(graph_data, indent=4))
+
+    #print(json.dumps(results, indent=4))
+
 
 lambda_handler({}, {})
